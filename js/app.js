6ae60b6f62218800c75f4d87ef647055c19c55dd
Aleksandra Kurek <kurek.aleksandra@gmail.com>
2016-06-07 15:23:55 +0200

    slider for 1 person - necessary for later

--- js/app.js
+++ js/app.js
@@ -39,3 +39,49 @@ $(window).on("scroll",function(){
 		$("#sticky").removeClass("positionTop");
 	}
 });
+
+//Slider
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    var prevButton = document.getElementById("prevPicture");
+    var nextButton = document.getElementById("nextPicture");
+    var pictures = document.querySelectorAll(".slider li");
+    var currentVisiblePicture = 0; // na poczatku bedzie widoczny zerowy obrazek
+		
+    // console.log(prevButton);
+    // console.log(nextButton);
+    // console.log(pictures);
+
+    pictures[currentVisiblePicture].classList.add("visible");
+
+    prevButton.addEventListener("click", function(event) {
+        console.log("Click in prev button");
+        pictures[currentVisiblePicture].classList.remove("visible");
+        currentVisiblePicture--;
+
+        if (currentVisiblePicture < 0) {
+            currentVisiblePicture = pictures.length - 1;
+        }
+
+        pictures[currentVisiblePicture].classList.add("visible");
+
+    });
+
+    nextButton.addEventListener("click", function(event) {
+        console.log("Click in next button");
+        pictures[currentVisiblePicture].classList.remove("visible");
+        currentVisiblePicture++;
+
+        if (currentVisiblePicture > (pictures.length - 1)) {
+            currentVisiblePicture = 0;
+        }
+
+        pictures[currentVisiblePicture].classList.add("visible");
+    });
+
+
+
+
+
+});
